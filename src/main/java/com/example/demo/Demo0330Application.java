8676d94c1d8fada1d738ed254d53be806c3ff9ea
arazin12 <68257911+arazin12@users.noreply.github.com>
2022-03-30 11:51:43 +0900

    0330_1

---
+++ src/main/java/com/example/demo/Demo0330Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo0330Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo0330Application.class, args);
+	}
+
+}
